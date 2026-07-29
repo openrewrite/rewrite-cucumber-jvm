785ee25e86434c0ef50eceb21113ef6ba42370c9
Tim te Beek <tim@moderne.io>
2026-07-29 09:47:46 +0200

    Upgrade Cucumber dependencies to latest release (#37)
    * Upgrade Cucumber dependencies to latest release
    
    Updated Cucumber dependencies to use the latest release versions.
    
    * Pin junit-platform-suite-api to the Java 8 compatible 1.x line

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,12 @@ description = "Cucumber JVM Migration"
 
 val rewriteVersion = rewriteRecipe.rewriteVersion.get()
 dependencies {
-    implementation("io.cucumber:cucumber-java:7.34.4")
-    implementation("io.cucumber:cucumber-java8:7.34.4")
-    implementation("io.cucumber:cucumber-plugin:7.34.4")
-    implementation("io.cucumber:cucumber-junit-platform-engine:7.34.4")
-    implementation("org.junit.platform:junit-platform-suite-api:1.14.2")
+    implementation("io.cucumber:cucumber-java:latest.release")
+    implementation("io.cucumber:cucumber-java8:latest.release")
+    implementation("io.cucumber:cucumber-plugin:latest.release")
+    implementation("io.cucumber:cucumber-junit-platform-engine:latest.release")
+    // JUnit Platform 6.x requires Java 17; recipe modules still compile against Java 8
+    implementation("org.junit.platform:junit-platform-suite-api:1.+")
 
     compileOnly("org.projectlombok:lombok:latest.release")
     annotationProcessor("org.projectlombok:lombok:latest.release")
