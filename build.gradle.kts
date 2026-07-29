bfe4d5912c4a9e8fe42e5a1cfdb884bed90fe48f
Tim te Beek <tim@moderne.io>
2026-07-29 09:29:26 +0200

    Pin junit-platform-suite-api to the Java 8 compatible 1.x line

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,8 @@ dependencies {
     implementation("io.cucumber:cucumber-java8:latest.release")
     implementation("io.cucumber:cucumber-plugin:latest.release")
     implementation("io.cucumber:cucumber-junit-platform-engine:latest.release")
-    implementation("org.junit.platform:junit-platform-suite-api:latest.release")
+    // JUnit Platform 6.x requires Java 17; recipe modules still compile against Java 8
+    implementation("org.junit.platform:junit-platform-suite-api:1.+")
 
     compileOnly("org.projectlombok:lombok:latest.release")
     annotationProcessor("org.projectlombok:lombok:latest.release")
